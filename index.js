c2785ceb9867cdf8707817ba0eba569290ffc914
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-02-22 18:56:47 -0500

    Add modal layout to JS logic and add close button

--- index.js
+++ index.js
@@ -7,28 +7,39 @@ modalButton.textContent = 'Lo quiero!';
 
 const modalContent = document.createElement('div');
 modalContent.classList.add('modal__content');
+
+modalContent.innerHTML = `
+  <img class="close-modal" onclick="removeModal()" src="/public/icons8-macos-close-32.png" alt="close modal icon" />
+  <input type="radio" name="figure-selection" id="box-1">
+  <input type="radio" name="figure-selection" id="box-2">
+  <input type="radio" name="figure-selection" id="box-3">
+  <div class="figures">
+    <label for="box-1"><img src="/public/spiderman.png" alt="Spiderman" /></label>
+    <label for="box-2"><img src="/public/robin.png" alt="Robin" /></label>
+    <label for="box-3"><img src="/public/batman.png" alt="Batman" /></label>
+  </div>
+`
 modalContent.appendChild(modalButton);
 
 modalContent.addEventListener('click', e => {
   e.stopPropagation();
 });
 
+function removeModal() {
+  setTimeout(() => {
+    page.removeChild(modal);
+  }, 200);
+}
+
 const modal = document.createElement('section');
 modal.classList.add('modal');
 modal.appendChild(modalContent);
 
-modal.addEventListener('click', () => {
-  setTimeout(() => {
-    page.removeChild(modal);
-  }, 200);
-});
+modal.addEventListener('click', removeModal);
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    console.log(button);
     page.append(modal);
     modalContent.focus();
   });
 });
-
-console.log(buttons);
