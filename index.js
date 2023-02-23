74de3d0bea1ce0e59e92d00b7000771e9bcc1949
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-02-23 00:41:03 -0500

    Log nice github repo message to the console

--- index.js
+++ index.js
@@ -61,3 +61,8 @@ buttons.forEach(button => {
     }
   });
 });
+
+console.log(
+  '%c Hey there! \n You can heck out the repo of this project here: \n https://github.com/salvarez96/animations-practice-course ', 
+  "font-size: 18px; font-weight: bold; background-color: rgba(255,255,0,0.3); text-shadow: #000;"
+);
